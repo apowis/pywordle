241264fff9910fa14c40ea62deda0653ace91e2a
Adam Powis <adam.powis@immersivelabs.com>
2022-01-28 16:22:15 +0000

    added argparse

--- run_game.py
+++ run_game.py
@@ -1,6 +1,8 @@
 """
 Plays a game of Wordle
 """
+import argparse
+from ast import arg
 from random import choice
 import sys
 
@@ -33,13 +35,21 @@ def run_game(answer):
 
 if __name__ == "__main__":
 
-    with open('/usr/share/dict/words', 'r') as words:
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-w', '--word', type=str, help='The word to guess.')
+    group.add_argument('-l', '--length', type=int, help='The length of the random word to be chosen.')
+    parser.add_argument('-d', '--dictionary', help='Location of local dictionary file', default='/usr/share/dict/words')
+    args = parser.parse_args()
+
+    word_length = args.length if args.length else len(args.word)
+    with open(args.dictionary, 'r') as words:
         for line in words:
-            if len(line) == int(sys.argv[1]):
+            if len(line) - 1 == word_length:
                 WORDS.append(
                     line.strip().lower()
                 )
 
-    answer = choice(WORDS)
+    answer = args.word if args.word else choice(WORDS)
     score = run_game(answer)
     print(f"Guessed '{answer}' in {score} shots")
